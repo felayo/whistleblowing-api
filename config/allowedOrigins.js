c2b88a35489e7603d24cc6a0b00ca3eb252d8169
felayo <ajibaye@gmail.com>
2025-10-21 11:37:17 +0100

    cors allowed origin updated

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,5 @@
 const allowedOrigins = [
-    'http://localhost:3000',
+    "http://localhost:5173",
     'https://www.ayorindeajibaye.com/portal',
     'http://127.0.0.1:5173',
     'https://felayotechworld.com',
